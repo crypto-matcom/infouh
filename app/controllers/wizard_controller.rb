46e8b74745065df8c31e965f8dcb100a73af3493
Amaury Diaz <a.caballero@estudiantes.matcom.uh.cu>
2017-05-10 21:40:45 -0400

    Created Wizard controller actions.

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -4,6 +4,22 @@ class WizardController < ApplicationController
     @queryWizard = ::QueryWizard.new
   end
 
+  def perform
+
+  end
+
+  def tables
+
+  end
+
+  def columns
+
+  end
+
+  def build
+
+  end
+
   def test
     puts @queryWizard.ConformQuery Source.all.first().ConnectionString, params[:prefix]
   end
